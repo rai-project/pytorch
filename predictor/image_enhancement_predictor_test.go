6fd7ace268a6ea9d99332ec7bbe37a3d3732b06c
abhiutd <abhibits2012@gmail.com>
2019-04-22 14:44:44 -0500

    update

--- predictor/image_enhancement_predictor_test.go
+++ predictor/image_enhancement_predictor_test.go
@@ -1,106 +1,108 @@
 package predictor
 
-// import (
-// 	"bytes"
-// 	"context"
-// 	goimage "image"
-// 	"os"
-// 	"path/filepath"
-// 	"testing"
+import (
+	"bytes"
+	"context"
+	goimage "image"
+	"os"
+	"path/filepath"
+	"testing"
 
-// 	"github.com/k0kubun/pp"
-// 	"github.com/rai-project/dlframework"
+	"github.com/k0kubun/pp"
+	"github.com/rai-project/dlframework"
 
-// 	"github.com/rai-project/dlframework/framework/options"
-// 	"github.com/rai-project/image"
-// 	"github.com/rai-project/image/types"
-// 	nvidiasmi "github.com/rai-project/nvidia-smi"
-// 	py "github.com/rai-project/pytorch"
-// 	"github.com/stretchr/testify/assert"
-// 	gotensor "gorgonia.org/tensor"
-// )
+	"github.com/rai-project/dlframework/framework/options"
+	"github.com/rai-project/image"
+	"github.com/rai-project/image/types"
+	nvidiasmi "github.com/rai-project/nvidia-smi"
+	py "github.com/rai-project/pytorch"
+	"github.com/stretchr/testify/assert"
+	gotensor "gorgonia.org/tensor"
+)
 
-// func TestImageEnhancement(t *testing.T) {
-// 	py.Register()
-// 	model, err := py.FrameworkManifest.FindModel("srgan:1.0")
-// 	assert.NoError(t, err)
-// 	assert.NotEmpty(t, model)
+func TestImageEnhancement(t *testing.T) {
+	py.Register()
+	model, err := py.FrameworkManifest.FindModel("srgan_v1.0:1.0")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, model)
 
-// 	device := options.CPU_DEVICE
-// 	if nvidiasmi.HasGPU {
-// 		device = options.CUDA_DEVICE
-// 	}
+	device := options.CPU_DEVICE
+	if nvidiasmi.HasGPU {
+		device = options.CUDA_DEVICE
+	}
 
-// 	batchSize := 1
-// 	ctx := context.Background()
-// 	opts := options.New(options.Context(ctx),
-// 		options.Device(device, 0),
-// 		options.BatchSize(batchSize))
+	batchSize := 1
+	ctx := context.Background()
+	opts := options.New(options.Context(ctx),
+		options.Device(device, 0),
+		options.BatchSize(batchSize))
 
-// 	predictor, err := NewImageEnhancementPredictor(*model, options.WithOptions(opts))
-// 	assert.NoError(t, err)
-// 	assert.NotEmpty(t, predictor)
-// 	defer predictor.Close()
+	predictor, err := NewImageEnhancementPredictor(*model, options.WithOptions(opts))
+	assert.NoError(t, err)
+	assert.NotEmpty(t, predictor)
+	defer predictor.Close()
 
-// 	imgDir, _ := filepath.Abs("./_fixtures")
-// 	imgPath := filepath.Join(imgDir, "penguin.png")
-// 	r, err := os.Open(imgPath)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	img, err := image.Read(r)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	imgDir, _ := filepath.Abs("./_fixtures")
+	imgPath := filepath.Join(imgDir, "penguin.png")
+	r, err := os.Open(imgPath)
+	if err != nil {
+		panic(err)
+	}
 
-// 	input := make([]*gotensor.Dense, batchSize)
-// 	imgFloats, err := normalizeImageHWC(img.(*types.RGBImage), []float32{127.5, 127.5, 127.5}, 127.5)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	height := img.Bounds().Dy()
-// 	width := img.Bounds().Dx()
-// 	channels := 3
-// 	for ii := 0; ii < batchSize; ii++ {
-// 		input[ii] = gotensor.New(
-// 			gotensor.WithShape(height, width, channels),
-// 			gotensor.WithBacking(imgFloats),
-// 		)
-// 	}
+	preprocessOpts, err := predictor.GetPreprocessOptions()
+	assert.NoError(t, err)
+	channels := preprocessOpts.Dims[0]
+	height := preprocessOpts.Dims[1]
+	width := preprocessOpts.Dims[2]
+	mode := preprocessOpts.ColorMode
 
-// 	err = predictor.Predict(ctx, input)
-// 	assert.NoError(t, err)
-// 	if err != nil {
-// 		return
-// 	}
+	var imgOpts []raiimage.Option
+	if mode == types.RGBMode {
+		imgOpts = append(imgOpts, raiimage.Mode(types.RGBMode))
+	} else {
+		imgOpts = append(imgOpts, raiimage.Mode(types.BGRMode))
+	}
 
-// 	pred, err := predictor.ReadPredictedFeatures(ctx)
-// 	assert.NoError(t, err)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	img, err := raiimage.Read(r, imgOpts...)
+	if err != nil {
+		panic(err)
+	}
 
-// 	f, ok := pred[0][0].Feature.(*dlframework.Feature_Image)
-// 	if !ok {
-// 		panic("expecting an image feature")
-// 	}
+	imgOpts = append(imgOpts, raiimage.Resized(height, width))
+	imgOpts = append(imgOpts, raiimage.ResizeAlgorithm(types.ResizeAlgorithmLinear))
+	resized, err := raiimage.Resize(img, imgOpts...)
 
-// 	// hrimg, err := image.Read((bytes.NewReader(f.Image.GetData())))
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
+	input := make([]*gotensor.Dense, batchSize)
+	imgFloats, err := normalizeImageHWC(resized, preprocessOpts.MeanImage, preprocessOpts.Scale)
+	if err != nil {
+		panic(err)
+	}
 
-// 	hrimg, _, _ := goimage.Decode(bytes.NewReader(f.Image.GetData()))
+	for ii := 0; ii < batchSize; ii++ {
+		input[ii] = gotensor.New(
+			gotensor.WithShape(channels, height, width),
+			gotensor.WithBacking(imgFloats),
+		)
+	}
 
-// 	pp.Println(hrimg.At(0, 0))
-// 	// jpeg.Decode(output, hrimg)
-// 	// pp.Println(bytes[0])
-// 	// assert.InDelta(t, float32(0.520469), intMask[72122], 0.001)
+	err = predictor.Predict(ctx, input)
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
 
-// 	// output, _ := os.Create("/tmp/output.png")
-// 	// defer output.Close()
-// 	// err = png.Encode(output, hrimg)
-// 	// if err != nil {
-// 	// 	panic(err)
-// 	// }
-// }
+	pred, err := predictor.ReadPredictedFeatures(ctx)
+	assert.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
+
+	f, ok := pred[0][0].Feature.(*dlframework.Feature_Image)
+	if !ok {
+		panic("expecting an image feature")
+	}
+
+	hrimg, _, _ := goimage.Decode(bytes.NewReader(f.Image.GetData()))
+
+	pp.Println(hrimg.At(0, 0))
+}
